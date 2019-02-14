ac2312edf58d7c6b22461c39e38040bf5fbabd77
zmg02 <1225798710@qq.com>
2019-02-14 09:18:28 +0000

    Finish static pages

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,6 +8,16 @@ class StaticPagesController extends Controller
 {
     public function home()
     {
-    	return '主页';
+    	return view('static_pages.home');
+    }
+
+    public function help()
+    {
+    	return view('static_pages.help');
+    }
+
+    public function about()
+    {
+    	return view('static_pages.about');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
