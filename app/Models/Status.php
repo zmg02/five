4f5881e548fe03aedf779e47a6ed18cbd55536ec
zmg02 <1225798710@qq.com>
2019-02-22 06:53:57 +0000

    用户微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    public funciton user()
+    public function user()
     {
     	return $this->belongsTo(User::class);
     }
--- database/migrations/2019_02_22_112417_create_statuses_table.php
+++ database/migrations/2019_02_22_112417_create_statuses_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schem\Blueprint;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateStatusesTable extends Migration
